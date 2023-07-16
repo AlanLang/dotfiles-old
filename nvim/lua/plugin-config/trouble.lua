e1ecbfb81ad18d8c630d88cc84d2cc0e372a3c56
alan <langwdalan@gmail.com>
2023-07-16 10:06:38 +0800

    update nvim trouble config

--- nvim/lua/plugin-config/trouble.lua
+++ nvim/lua/plugin-config/trouble.lua
@@ -4,6 +4,19 @@ return {
   lazy = true,
   cmd = { "TroubleToggle", "Trouble", "TroubleRefresh" },
   config = function()
-    require("trouble").setup()
+    require("trouble").setup({
+      auto_open = false,
+      auto_close = true,
+      auto_preview = true,
+      auto_fold = false,
+      signs = {
+        error = " ",
+        warning = " ",
+        hint = " ",
+        information = " ",
+        other = "﫠",
+      },
+      use_lsp_diagnostic_signs = true,
+    })
   end,
 }
